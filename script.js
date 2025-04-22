2d6f57ffe26c97a50476f2d5747cdc3be19f0896
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2025-04-22 03:12:06 -0400

    forgot some stufff

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ const tracks = [
   new Audio('audio/slide6.mp3'),
   new Audio('audio/slide7.mp3'),
   new Audio('audio/slide8.mp3'),
-  new Audio('audio/slide9.mp3'),
-  new Audio('audio/slide10.mp3')
+  new Audio('audio/slide9.mp3')
 ];
 tracks.forEach(t => {
   t.loop   = true;  // keep playing while the slide is up
